3983136010727ce3480dc4c2e339770ba4eea749
Dipeshbist <dipeshbist150@gmail.com>
2025-02-24 08:50:42 +0545

    Updated Files

--- config/db.js
+++ config/db.js
@@ -2,10 +2,19 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const mongoURL = process.env.MONGO_URL || process.env.MONGO_URL_LOCAL;
-mongoose
-  .connect(mongoURL)
-  .then(() => console.log("Connected to MongoDB server"))
-  .catch((err) => console.error("MongoDB connection error:", err));
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(mongoURL);
+    console.log("Connected to MongoDB server");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1); // Exit the process on failure
+  }
+};
+
+// Call the function to connect
+connectDB();
 
 const db = mongoose.connection;
 
